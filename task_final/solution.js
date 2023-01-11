8669c30bbaab4edc0906f0b1f0a62f826c40e66b
Evgeniya Popova <vinner-e@yandex.ru>
2023-01-11 20:41:14 +0300

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,24 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: {name, phone}, order: {address,sum}, goods: []};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({title:goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+   // data.order.address = address;
+   data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж," + " кв. " + address.flat;
+   // data.order.sum = name + phone + address + goods + sum;
+   data.order.sum = sum;
+   // data.client = 'Иван';
+   data.client = name + " " + phone;
 
-    data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+
+    let jsonData = JSON.stringify({data:data});
 
     return jsonData;
 }
